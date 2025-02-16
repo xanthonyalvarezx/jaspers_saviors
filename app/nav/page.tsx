91852f4da9784f54c537eaeddb24e09b3057e7cc
jasperAnonymouse <xanthonyalvarezx@gmail.com>
2025-02-16 16:57:51 -0500

    home page started

--- app/nav/page.tsx
+++ app/nav/page.tsx
@@ -1,18 +1,26 @@
 import Link from 'next/link'
 import React from 'react'
-
+import styles from './nav.module.css'
+import Image from 'next/image'
+import Logo from '../assets/jasperssaviorslogo.png'
 const Navbar = () => {
     return (
-        <div className='p-3 m-3 position-static'>
-            <nav>
+        <div className='navbar p-3 m-3 position-static'>
+            <nav className={styles.nav}>
+
                 <ul className='flex justify-between'>
-                    <Link href='/'>HOME</Link>
-                    <Link href='/'>DONATE</Link>
-                    <Link href='/'>CONTACT</Link>
-                    <Link href='/'>ABOUT JASPERS SAVIORS</Link>
+                    <Image
+                        src={Logo}
+                        width={200}
+                        height={200}
+                        alt="Picture of the author"
+                    />
+                    <h3><li><Link className={styles.navlink} href='/'>HOME</Link></li></h3>
+                    <h3><li><Link className={styles.navlink} href='/'>DONATE</Link></li></h3>
+                    <h3><li><Link className={styles.navlink} href='/'>CONTACT</Link></li></h3>
+                    <h3><li><Link className={styles.navlink} href='/'>ABOUT JASPERS SAVIORS</Link></li></h3>
                 </ul>
             </nav>
-
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import styles from './main.module.css'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="">
+    <div >
+      <section className="flex">
+        <div className="w-3/6 px-10 ">
+          <h3>Who are we</h3>
+          <p className='break-normal'>Lorem ipsum odor amet, consectetuer adipiscing elit. Senectus per libero placerat bibendum penatibus curabitur. Maximus lectus justo laoreet ut vitae justo porta condimentum. Et ullamcorper risus orci inceptos pharetra egestas. Suscipit sociosqu ullamcorper netus pretium accumsan vehicula aliquam. Ut eros lacinia elit phasellus vitae; arcu ridiculus. Ultricies ligula habitant finibus montes eleifend dapibus. Scelerisque ad ullamcorper turpis netus molestie porta.</p>
+        </div>
+        <div> <Image
+          src="/profile.png"
+          width={200}
+          height={200}
+          alt="Picture of the author"
+        /></div>
+      </section>
+
+
+
     </div>
   );
 }
