1bda719dd347b6c383d12d38ceb91b277244254c
Anthony Alvarez <anthonyalvarez@Anthonys-MacBook-Pro.local>
2025-02-22 09:06:05 -0500

    added prisma for mysql, and all needed pages with boilerplate

---
+++ app/donate/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Donate = () => {
+    return (
+        <div>
+            <h1>Donate</h1>
+        </div>
+    )
+}
+
+export default Donate
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,25 @@
 import styles from './main.module.css'
 import Image from 'next/image';
+import jasperb from './assets/jasper_blanket.jpg'
 
 export default function Home() {
   return (
-    <div className=''>
-      <section className="flex">
-        <div className="w-3/6 px-10">
-          <h3>Who are we</h3>
-          <p className='break-normal'>Lorem ipsum odor amet, consectetuer adipiscing elit. Senectus per libero placerat bibendum penatibus curabitur. Maximus lectus justo laoreet ut vitae justo porta condimentum. Et ullamcorper risus orci inceptos pharetra egestas. Suscipit sociosqu ullamcorper netus pretium accumsan vehicula aliquam. Ut eros lacinia elit phasellus vitae; arcu ridiculus. Ultricies ligula habitant finibus montes eleifend dapibus. Scelerisque ad ullamcorper turpis netus molestie porta.</p>
+    <div >
+      <section className='flex justify-center align-center p-5'>
+        <div className="flex-column w-3/6  justify-center align-center">
+          <div className='flex justify-center align-center'>
+            <h2>Who are we</h2>
+          </div>
+          <p >Lorem ipsum odor amet, consectetuer adipiscing elit. Senectus per libero placerat bibendum penatibus curabitur. Maximus lectus justo laoreet ut vitae justo porta condimentum. Et ullamcorper risus orci inceptos pharetra egestas. Suscipit sociosqu ullamcorper netus pretium accumsan vehicula aliquam. Ut eros lacinia elit phasellus vitae; arcu ridiculus. Ultricies ligula habitant finibus montes eleifend dapibus. Scelerisque ad ullamcorper turpis netus molestie porta.</p>
         </div>
-        <div> <Image
-          src="/profile.png"
-          width={200}
-          height={200}
-          alt="Picture of the author"
-        /></div>
+        <div className="flex w-3/6 px-10 justify-center align-center">
+          <Image
+            src={jasperb}
+            width={400}
+            height={800}
+            alt="Picture of the author"
+          /></div>
       </section>
-
-
-
     </div>
   );
 }
